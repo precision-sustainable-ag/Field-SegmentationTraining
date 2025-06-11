ec8efe5e227c392181dc2da5b14970997ba5817d
Navjot Singh <navjotnangia@gmail.com>
2025-06-11 10:37:03 -0400

    added early_train_stopping functionality for model training

---
+++ src/utils/early_stopping_train.py
@@ -0,0 +1,21 @@
+class EarlyStopping:
+    """
+    Early stops the training if validation loss doesn't improve after a given patience.
+    """
+    def __init__(self, patience=7, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+
+    def __call__(self, current_score):
+        if self.best_score is None:
+            self.best_score = current_score
+        elif current_score > self.best_score - self.min_delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = current_score
+            self.counter = 0
