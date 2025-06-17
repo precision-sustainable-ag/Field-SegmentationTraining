1104c32cf849eed67cc9de645ba0005de2ac6270
Sina <sbaghba@ncsu.edu>
2025-06-17 13:09:23 -0400

    progress so far. confused about configs

---
+++ src/models/lit_segmentation.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+import segmentation_models_pytorch as smp
+from torchmetrics.classification import BinaryJaccardIndex as IoU, BinaryF1Score as Dice
+
+
+
+class LitSegmentation(pl.LightningModule):
+    """
+    PyTorch Lightning wrapper for a segmentation_models.pytorch model.
+    Uses BCEWithLogitsLoss + IoU and Dice metrics.
+    """
+
+    def __init__(self, cfg: DictConfig):
+        super().__init__()
+        # Save hyperparams to self.hparams for logging
+        self.save_hyperparameters(cfg)
+
+        # Instantiate the SMP model from config
+        self.model: torch.nn.Module = smp.Unet(
+            encoder_name=cfg.model.encoder_name,
+            encoder_weights=cfg.model.encoder_weights,
+            in_channels=cfg.model.in_channels,
+            classes=cfg.model.classes,
+            activation=None  # logits output
+        )
+
+        # Loss
+        self.loss_fn = torch.nn.BCEWithLogitsLoss()
+
+        # Metrics (binary segmentation: 2 classes [bg,fg])
+
+        # self.train_iou = IoU(num_classes=2, ignore_index=0)
+        # self.val_iou   = IoU(num_classes=2, ignore_index=0)
+        # self.train_dice = Dice(num_classes=2, ignore_index=0)
+        # self.val_dice   = Dice(num_classes=2, ignore_index=0)
+
+        self.train_iou = IoU()
+        self.val_iou   = IoU()
+        self.train_dice = Dice()
+        self.val_dice   = Dice()
+
+
+        # Learning rate
+        self.lr = cfg.train.learning_rate
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        imgs, masks = batch
+        logits = self(imgs)
+        loss = self.loss_fn(logits, masks)
+
+        # Compute metrics on the batch
+        preds = (torch.sigmoid(logits) > 0.5).long()
+        self.train_iou.update(preds, masks.long())
+        self.train_dice.update(preds, masks.long())
+
+        # Log
+        self.log("train/loss", loss, on_step=False, on_epoch=True)
+        self.log("train/iou",   self.train_iou,   on_step=False, on_epoch=True)
+        self.log("train/dice",  self.train_dice,  on_step=False, on_epoch=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        imgs, masks = batch
+        logits = self(imgs)
+        loss = self.loss_fn(logits, masks)
+
+        preds = (torch.sigmoid(logits) > 0.5).long()
+        self.val_iou.update(preds, masks.long())
+        self.val_dice.update(preds, masks.long())
+
+        self.log("val/loss", loss, on_step=False, on_epoch=True)
+        self.log("val/iou",  self.val_iou,  on_step=False, on_epoch=True)
+        self.log("val/dice", self.val_dice, on_step=False, on_epoch=True)
+
+        return loss
+
+    def configure_optimizers(self):
+        # Instantiate optimizer and scheduler from cfg
+        optimizer = torch.optim.AdamW(
+            self.parameters(),
+            lr=self.lr,
+            weight_decay=self.hparams.train.optimizer.weight_decay
+        )
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer,
+            T_max=self.hparams.train.scheduler.T_max,
+            eta_min=self.hparams.train.scheduler.eta_min
+        )
+        return [optimizer], [scheduler]
---
+++ src/train.py
@@ -0,0 +1,72 @@
+# src/train.py
+
+from omegaconf import DictConfig
+import hydra
+from torch.utils.data import random_split, DataLoader
+from models.lit_segmentation import LitSegmentation
+from data.dataset import FieldDataset
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
+
+@hydra.main(config_path="../conf", config_name="config")
+def train_entry(cfg: DictConfig):
+    """
+    Entry point for training mode.
+    Usage: python train.py +pipeline.mode=train
+    """
+    # 1) Build dataset + splits
+    full = FieldDataset(cfg, mode="train")
+    n_train = int(len(full) * cfg.train.train_val_split)
+    n_val   = len(full) - n_train
+    train_ds, val_ds = random_split(full, [n_train, n_val], generator=torch.Generator().manual_seed(cfg.train.seed))
+
+    # 2) DataLoaders
+    train_loader = DataLoader(train_ds, batch_size=cfg.train.batch_size, shuffle=True,
+                              num_workers=cfg.train.num_workers, pin_memory=cfg.train.pin_memory)
+    val_loader   = DataLoader(val_ds,   batch_size=cfg.train.batch_size, shuffle=False,
+                              num_workers=cfg.train.num_workers, pin_memory=cfg.train.pin_memory)
+
+    # 3) Model
+    model = LitSegmentation(cfg)
+
+    # 4) Logger
+    loggers = [hydra.utils.instantiate(lcfg) for lcfg in cfg.train.logger]
+    logger = WandbLogger(
+        project=cfg.train.logger.project,
+        entity=cfg.train.logger.entity,
+        log_model=cfg.train.logger.log_model
+    )
+
+    # 5) Callbacks
+    ckpt_conf = cfg.train.checkpoint
+    checkpoint_cb = ModelCheckpoint(
+        monitor=ckpt_conf.monitor,
+        mode=ckpt_conf.mode,
+        save_top_k=ckpt_conf.save_top_k,
+        dirpath=ckpt_conf.dirpath
+    )
+    es_conf = cfg.train.early_stop
+    earlystop_cb = EarlyStopping(
+        monitor=es_conf.monitor,
+        mode=es_conf.mode,
+        patience=es_conf.patience
+    )
+
+    # 6) Trainer
+    trainer = Trainer(
+        accelerator=cfg.train.trainer.accelerator,
+        devices=cfg.train.trainer.devices,
+        precision=cfg.train.trainer.precision,
+        max_epochs=cfg.train.max_epochs,
+        deterministic=cfg.train.trainer.deterministic,
+        logger=logger,
+        callbacks=[checkpoint_cb, earlystop_cb],
+        default_root_dir=cfg.paths.model_save_dir
+    )
+
+    # 7) Fit
+    trainer.fit(model, train_loader, val_loader)
+
+if __name__ == "__main__":
+    train_entry()
