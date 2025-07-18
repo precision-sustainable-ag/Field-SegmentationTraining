a79181e1a906e94849b34b35d025ce49b63861d7
Matthew Kutugata <mattkutugata@gmail.com>
2025-07-18 16:48:23 -0400

    exr and quick fixex to inspect

--- src/mask_gen_utils/missing_red.py
+++ src/mask_gen_utils/missing_red.py
@@ -9,38 +9,53 @@ class MissingRed:
     """
     
     def __init__(self, missing_red_cfg: DictConfig):
-        self.red_missing_hsv_lower = np.array(missing_red_cfg.hsv_lower, dtype=np.uint8)
-        self.red_missing_hsv_upper = np.array(missing_red_cfg.hsv_upper, dtype=np.uint8)
-        self.red_missing_hsv_lower_2 = np.array(missing_red_cfg.hsv_lower_2, dtype=np.uint8)
-        self.red_missing_hsv_upper_2 = np.array(missing_red_cfg.hsv_upper_2, dtype=np.uint8)
+        self.exr_thresh = missing_red_cfg.exr_thresh  # default if not set
+        self.saturation_thresh = missing_red_cfg.saturation_thresh  # default to 0 to always boost, but can be >0    
         self.red_opening_size = missing_red_cfg.opening_kernel_size
         self.red_closing_size = missing_red_cfg.closing_kernel_size
         self.red_erosion_size = missing_red_cfg.erosion_kernel_size
 
     def process_missing_red(self, image: np.ndarray, cropout_mask: np.ndarray) -> np.ndarray:
         """
-        Generate a binary mask for detecting red-colored regions using HSV thresholding.
+        Generate a binary mask for detecting red-colored regions using Excess Red (ExR) boosted by saturation.
 
         Args:
-            image (np.ndarray): RGB image.
+            image (np.ndarray): RGB image (OpenCV format).
+            cropout_mask (np.ndarray): Pre-existing mask to combine.
 
         Returns:
             np.ndarray: Binary mask with red-missing regions as 255.
         """
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        
-        refined_mask_for_red_1 = cv2.inRange(hsv_image, self.red_missing_hsv_lower, self.red_missing_hsv_upper)
-        refined_mask_for_red_2 = cv2.inRange(hsv_image, self.red_missing_hsv_lower_2, self.red_missing_hsv_upper_2)
-        refined_mask_red_final = cv2.bitwise_or(refined_mask_for_red_1, refined_mask_for_red_2)
-        
-        morphcleaned_refined_mask_red_final = MorphCleanedMask.morph_cleaned_mask(
-            refined_mask_red_final, 
-            self.red_opening_size, 
-            self.red_closing_size, 
+        # Calculate ExR
+        R = image[:, :, 0].astype(np.float32)
+        G = image[:, :, 1].astype(np.float32)
+        B = image[:, :, 2].astype(np.float32)
+        exr = 2 * R - G - B
+
+        # Calculate normalized saturation and mask out very gray pixels if desired
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        s = hsv[...,1].astype(np.float32)
+        s_norm = s / 255.0
+        if self.saturation_thresh > 0:
+            color_mask = (s >= self.saturation_thresh).astype(np.float32)
+            s_norm = s_norm * color_mask  # Optional: set normalized S to zero if below threshold
+
+        # Boost ExR by normalized saturation
+        exr_boosted = exr * s_norm
+
+        # Threshold ExR (boosted)
+        exr_mask = np.where(exr_boosted > self.exr_thresh, 255, 0).astype(np.uint8)
+
+        # Morphological cleaning
+        morphcleaned_exr_mask = MorphCleanedMask.morph_cleaned_mask(
+            exr_mask,
+            self.red_opening_size,
+            self.red_closing_size,
             self.red_erosion_size
-            )
-        
-        combined_refined_mask = cv2.bitwise_or(cropout_mask, morphcleaned_refined_mask_red_final)
+        )
+
+        # Combine with cropout_mask
+        combined_refined_mask = cv2.bitwise_or(cropout_mask, morphcleaned_exr_mask)
         combined_refined_mask_binary = np.where(combined_refined_mask > 0, 255, 0).astype(np.uint8)
 
         return combined_refined_mask_binary
