80029ca7013ad05367f132989b569accc66a0592
Matthew Kutugata <mattkutugata@gmail.com>
2025-08-07 11:00:39 -0400

    Skeleton filter_by_species.py changes. Non-workling changes. filter_by_species.py needs updating.

--- src/create_project.py
+++ src/create_project.py
@@ -10,15 +10,15 @@ log = logging.getLogger(__name__)
 def create_project(cfg: DictConfig) -> None:
     """ Main entry point for creating a new project """
     log.info(f"Creating a project at {cfg.paths.project_dir}")
+    
+    # Filter images by species and get a DataFrame of sampled images
     g = GroupImagesBySpecies(cfg)
     sampled_species = g.get_sampled_images()
-
+    
+    # Create the project directory structure and copy images, and save the temporary database
     create_project = CreateProject(cfg)
-    create_project.copy_from_lts_to_local(sampled_species)
-
-
-    #TODO: After users are done with their mask corrections, it needs to be saved to lts saved directory
-
+    sampled_df = create_project.copy_from_lts_to_local(sampled_species)
+    create_project.save_temp_db(sampled_df)
 
 if __name__ == "__main__":
     create_project()
